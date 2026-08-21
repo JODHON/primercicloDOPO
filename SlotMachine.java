82f9273124b73b55c4b093bd4c55bbaca62c6597
JODHON <josedavidhorta@gmail.com>
2026-08-21 16:42:31 -0500

    añadir proyecto shapes

---
+++ SlotMachine.java
@@ -0,0 +1,90 @@
+
+/**
+ * Write a description of class SlotMachine here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SlotMachine
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SlotMachine
+     */
+    public SlotMachine()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    
+    public void addWheel(int pos){
+        
+    }
+    
+    public void delWheel(int pos){
+        
+    
+    }
+    
+    public void addSymbol(String Symbol){
+        
+    }
+    
+    public void delSymbol(String Symbol){
+        
+    }
+    
+    public void placeSymbol(int wheel, String Symbol){
+        
+    }
+    
+    public void spin(){
+        
+    }
+    
+    public String symbols(){
+        
+    }
+    
+    public int disctintSymbols(){
+        
+    }
+    
+    public String configuration(){
+        
+    }
+    
+    public boolean isJackpot(){
+        
+    }
+    
+    public void makeVisible(){
+        
+    }
+    
+    public void makeInvisible(){
+        
+    }
+    
+    public void exit(){
+        
+    }
+    
+    public boolean ok(){
+        
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
