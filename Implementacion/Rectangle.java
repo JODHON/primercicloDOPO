97718060f5b379e4a53d003fdc85224dd044b8b7
JODHON <josedavidhorta@gmail.com>
2026-08-21 22:22:44 -0500

    diagrama astah

--- Implementacion/Rectangle.java
+++ Implementacion/Rectangle.java
@@ -6,8 +6,6 @@ import java.awt.*;
  * @author  Michael Kolling and David J. Barnes (Modified)
  * @version 1.0  (15 July 2000)()
  */
-
-
  
 public class Rectangle{
 
@@ -32,7 +30,6 @@ public class Rectangle{
         isVisible = false;
     }
     
-
     /**
      * Make this rectangle visible. If it was already visible, do nothing.
      */
@@ -49,34 +46,6 @@ public class Rectangle{
         isVisible = false;
     }
     
-    /**
-     * Move the rectangle a few pixels to the right.
-     */
-    public void moveRight(){
-        moveHorizontal(20);
-    }
-
-    /**
-     * Move the rectangle a few pixels to the left.
-     */
-    public void moveLeft(){
-        moveHorizontal(-20);
-    }
-
-    /**
-     * Move the rectangle a few pixels up.
-     */
-    public void moveUp(){
-        moveVertical(-20);
-    }
-
-    /**
-     * Move the rectangle a few pixels down.
-     */
-    public void moveDown(){
-        moveVertical(20);
-    }
-
     /**
      * Move the rectangle horizontally.
      * @param distance the desired distance in pixels
@@ -95,46 +64,6 @@ public class Rectangle{
         erase();
         yPosition += distance;
         draw();
-    }
-
-    /**
-     * Slowly move the rectangle horizontally.
-     * @param distance the desired distance in pixels
-     */
-    public void slowMoveHorizontal(int distance){
-        int delta;
-
-        if(distance < 0) {
-            delta = -1;
-            distance = -distance;
-        } else {
-            delta = 1;
-        }
-
-        for(int i = 0; i < distance; i++){
-            xPosition += delta;
-            draw();
-        }
-    }
-
-    /**
-     * Slowly move the rectangle vertically.
-     * @param distance the desired distance in pixels
-     */
-    public void slowMoveVertical(int distance){
-        int delta;
-
-        if(distance < 0) {
-            delta = -1;
-            distance = -distance;
-        } else {
-            delta = 1;
-        }
-
-        for(int i = 0; i < distance; i++){
-            yPosition += delta;
-            draw();
-        }
     }
 
     /**
@@ -183,4 +112,3 @@ public class Rectangle{
         }
     }
 }
-
